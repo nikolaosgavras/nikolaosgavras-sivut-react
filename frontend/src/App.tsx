d4e24e87cac1c74826b0deac06b6e33a2e1e4028
nikolaosgavras <gavras.nikolaos@gmail.com>
2026-05-09 16:32:40 +0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { SiteHeader } from './components/SiteHeader'
+
+function App() {
+  
+  return (
+    <>
+      <head>
+        <title>Nikolaos Gavras - Kotisivu</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </head>
+
+      <body className='bg-slate-950'>
+        <SiteHeader />
+
+        
+      </body>
+    </>
+  )
+}
+
+export default App
