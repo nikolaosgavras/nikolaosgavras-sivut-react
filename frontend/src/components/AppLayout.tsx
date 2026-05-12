060d42a67e640274e8af942af6efe3fcfbc341b5
nikolaosgavras <gavras.nikolaos@gmail.com>
2026-05-12 17:44:36 +0300

    added background

--- frontend/src/components/AppLayout.tsx
+++ frontend/src/components/AppLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router"
 import { SiteHeader } from "./SiteHeader"
+import { MeshBackground } from "./MeshBackground"
 
 export const AppLayout = () => {
   return (
     <>
+      <MeshBackground />
       <SiteHeader />
       <main>
         <Outlet />
